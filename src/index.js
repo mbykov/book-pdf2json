f64a5292138e1ae4a21ace601015b57fb42ae552
Michael Bykov <m.bykov@gmail.com>
2020-12-16 01:24:45 +0300

    i

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ export async function pdf2json(bpath) {
 function parseText(str) {
   str = cleanStr(str)
   let pages = str.trim().split('PAGE_BREAK')
-  pages = pages.slice(0, 25)
+  // pages = pages.slice(0, 25)
   let cpages = []
 
   for (let page of pages) {
@@ -90,7 +90,6 @@ function parseText(str) {
   let possibleheads = pages.map(page=> page[0][0])
   let uniq = _.uniq(possibleheads)
   if (possibleheads.length/uniq.length > 10) has_colon = true
-  // log('_HAS_COLON_', possibleheads.length, uniq.length, has_colon)
 
   has_colon = true
 
@@ -101,23 +100,22 @@ function parseText(str) {
     }
     let max = _.max(freqs, 'freq')
     let colon = max.colon
-    // log('_COLON_', colon)
 
     for (let page of pages) {
       if (page[0][0] === colon) page[0] = page[0].slice(1)
     }
   }
+
+  //compact pages
   pages = pages.map(page=> {
     return page.filter(par=> par.length)
   })
 
   pages = pages.filter(page=> page.length)
-  // log('___PAGES___', pages)
 
   pages.forEach(page=> {
     page[0][0] = 'HEAD-' + page[0][0]
   })
-
 
   let cpars = [], row
   for (let page of pages) {
@@ -126,6 +124,7 @@ function parseText(str) {
       cpars.push(...breakRow(row))
     }
   }
+
   cpars = _.flattenDeep(cpars)
 
   // log('___CPARS___', cpars)
@@ -133,7 +132,6 @@ function parseText(str) {
   let text  = cpars.join('BREAK')
   text = text.replace(/BREAKHEAD-/, '')
   let mds = text.split('BREAK')
-  let titles = mds.filter(par=> /HEAD/.test(par) && par.length < 50)
   let docs = mds.map(par=> {
     let doc = {md: par}
     if (/HEAD-/.test(par)) {
@@ -144,89 +142,6 @@ function parseText(str) {
   })
 
   return docs
-}
-
-
-export async function pdf2json_(bpath) {
-  return new Promise(function (resolve, reject) {
-    let pdfParser = new PDFParser(this, 1)
-    pdfParser.on("pdfParser_dataReady", function(evtData) {
-      // let meta = pdfParser.prototype.loadMetaData ()
-      // log('__M', meta)
-      let str = pdfParser.getRawTextContent()
-      str = str.replace(/\r/g, '')
-      let rows = str.split('\n')
-      let pages = [], page = [], clean
-      rows.forEach(row=> {
-        if (/Page.*Break/.test(row)) {
-          if (page.length) pages.push(page)
-          page = []
-        } else {
-          clean = row.trim()
-          if (!clean) return
-          clean = cleanStr(clean)
-          page.push(clean)
-        }
-      })
-      // pages = pages.slice(68, 71)
-
-      // remove digits-only colons:
-      let cleans = [], test
-      for (let page of pages) {
-        test = page[0].replace(/-/g, '').trim()
-        if (/^\d+$/.test(test)) page = page.slice(1)
-        test = page[page.length-1].replace(/-/g, '').trim()
-        if (/^\d+$/.test(test)) page = page.slice(0, -1)
-        cleans.push(page)
-      }
-
-      // remove possible colons:
-      let has_colon = false
-      let singles = cleans.map(page=> page[0])
-      let uniq = _.uniq(singles)
-      if (singles.length/uniq.length > 10) has_colon = true
-      // log('_HAS_COLON_', singles.length, uniq.length, has_colon)
-      let freqs = []
-      if (has_colon) {
-        for (let colon of uniq) {
-          freqs.push({colon, freq: countInArray(singles, colon)})
-        }
-        let max = _.max(freqs, 'freq')
-        let colon = max.colon
-
-        pages = []
-        for (let clean of cleans) {
-          if (clean[0] == colon) page = clean.slice(1)
-          pages.push(page)
-        }
-      } else {
-        pages = cleans
-      }
-      pages.forEach(page=> {
-        page[0] = 'HEAD-' + page[0]
-      })
-     let strs = _.flatten(pages)
-      let text = strs.join('\n')
-
-      let pars = breakRow(text)
-      let titles = pars.filter(par=> /HEAD/.test(par) && par.length < 50)
-      let docs = pars.map(par=> {
-        let doc = {md: par}
-        if (/HEAD/.test(par) && par.length < 50) {
-          doc.level = 2
-          doc.md = par.slice(5)
-        }
-        return doc
-      })
-      let descr = {title: 'xxx', auth: 'xxx'}
-      let res = {descr, docs, imgs: []}
-      resolve(res)
-    })
-    pdfParser.on("pdfParser_dataError", function(evtData) {
-      log('____ERR:')
-    })
-    pdfParser.loadPDF(bpath)
-  })
 }
 
 function cleanStr(str) {

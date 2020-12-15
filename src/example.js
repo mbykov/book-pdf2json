6db0bc8eafa9019713799d8ea006505b7241338e
Michael Bykov <m.bykov@gmail.com>
2020-12-15 16:32:14 +0300

    i

--- src/example.js
+++ src/example.js
@@ -10,11 +10,11 @@ import { pdf2json } from "./index";
 
 let bpath = 'text-only.pdf'
 // bpath = 'images.pdf'
-bpath = 'lakatos.pdf'
+// bpath = 'lakatos.pdf'
 // bpath = 'bofre.pdf'
-bpath = 'warriors.pdf'
+// bpath = 'warriors.pdf'
 // bpath = 'typescript.pdf'
-bpath = 'the name of the rose.pdf'
+// bpath = 'the name of the rose.pdf'
 // bpath = '05.Harry Potter und der Orden des Phönix.pdf'
 // bpath = '1984.pdf'
 
@@ -24,14 +24,18 @@ log('RUN BPATH:', bpath)
 async function start(bpath, write) {
   let {descr, docs, imgs} = await pdf2json(bpath)
   if (!docs) {
-    log('_ERR:', descr)
+    log('_ERR-start:', descr)
     return
   }
 
   log('_descr:', descr)
+  // log('_docs:', docs)
   log('_docs:', docs.length)
   log('_imgs', imgs.length)
   // log('_slice', mds.slice(-10))
+
+  return
+
   let fns = docs.filter(doc=> doc.footnote)
   let refs = docs.filter(doc=> doc.refnote)
   log('_fns:', fns.length)
--- src/index.js
+++ src/index.js
@@ -5,23 +5,48 @@ const path = require("path")
 const log = console.log
 const fse = require('fs-extra')
 
-// let PDFParser = require("pdf2json")
-// const PDFExtract = require('pdf.js-extract').PDFExtract;
-// const pdfExtract = new PDFExtract();
 
 const pdf = require("pdf-extraction");
 
+let options = {
+  pagerender: render_page,
+};
+
+function render_page(pageData) {
+  let render_options = {
+    normalizeWhitespace: false,
+    disableCombineTextItems: false,
+  };
+
+  return pageData.getTextContent(render_options)
+    .then(function (textContent) {
+      let lastY,
+          text = "PAGE_BREAK";
+      for (let item of textContent.items) {
+        // log('_I', item)
+        if (lastY == item.transform[5] || !lastY) {
+          text += item.str;
+        } else {
+          text += "\n" + item.str;
+        }
+        lastY = item.transform[5];
+      }
+      return text;
+    })
+    .catch(err=> {
+      log('_ERR-text', err)
+    })
+}
+
 export async function pdf2json(bpath) {
   let dataBuffer = fse.readFileSync(bpath)
-  return pdf(dataBuffer)
+  return pdf(dataBuffer, options)
     .then(function (data) {
-      log('data.numpages', data.numpages);
-      log('data.numrender', data.numrender);
-      log('data.info', data.info);
-      // log('data.metadata', data.metadata);
-      log('data.version', data.version);
-      log('data.text', data.text.slice(0,100));
-      let res = {descr: data.info, docs:[], imgs: []}
+      // log('__data', _.keys(data));
+      let descr = {title: data.info.Title, author: data.info.Author}
+      let docs = parseText(data.text)
+      log('___docs', docs.length)
+      let res = {descr, docs, imgs: []}
       return res
     })
     .catch(err=> {
@@ -29,104 +54,105 @@ export async function pdf2json(bpath) {
     })
 }
 
-// export async function pdf2json__() {
-//   const options = {}; /* see below */
-//   let pdfpath = path.resolve(__dirname, '../test/text-only.pdf')
-//   return new Promise(function (resolve, reject) {
-//     pdfExtract.extract(pdfpath, options, (err, data) => {
-//       if (err) return log(err);
-//       // log('_DATA', data);
-//       let docs = data.pages[0].content.slice(0,1)
-//       let res = {descr: {}, docs, imgs: []}
-//       resolve(res)
-//     })
-//   })
-//   // return new Promise(function (resolve, reject) {
-//   //   resolve({descr: {}, docs: [], imgs: []})
-//   // })
-// }
+function parseText(str) {
+  let pages = str.split('PAGE_BREAK')
+  // pages = pages.map(page=> page.split('\n\n'))
+  let cpages = []
+  pages.forEach(page=> {
+    let pars = page.trim().replace(/ \n/g, '\n').split('\n\n')
+    log('___PAGE___', pars)
+    let cpars = _.flatten(pars.map(par=> parsePar(par)))
+  })
+  // let docs = data.text //.slice(20000, 20001)
+  return cpages
+}
 
-// export async function pdf2json_(bpath) {
-//   return new Promise(function (resolve, reject) {
-//     let pdfParser = new PDFParser(this, 1)
-//     pdfParser.on("pdfParser_dataReady", function(evtData) {
-//       // let meta = pdfParser.prototype.loadMetaData ()
-//       // log('__M', meta)
-//       let str = pdfParser.getRawTextContent()
-//       str = str.replace(/\r/g, '')
-//       let rows = str.split('\n')
-//       let pages = [], page = [], clean
-//       rows.forEach(row=> {
-//         if (/Page.*Break/.test(row)) {
-//           if (page.length) pages.push(page)
-//           page = []
-//         } else {
-//           clean = row.trim()
-//           if (!clean) return
-//           clean = cleanStr(clean)
-//           page.push(clean)
-//         }
-//       })
-//       // pages = pages.slice(68, 71)
+function parsePar(str) {
+  // log('___PAR___', str)
+  return []
+}
 
-//       // remove digits-only colons:
-//       let cleans = [], test
-//       for (let page of pages) {
-//         test = page[0].replace(/-/g, '').trim()
-//         if (/^\d+$/.test(test)) page = page.slice(1)
-//         test = page[page.length-1].replace(/-/g, '').trim()
-//         if (/^\d+$/.test(test)) page = page.slice(0, -1)
-//         cleans.push(page)
-//       }
+export async function pdf2json_(bpath) {
+  return new Promise(function (resolve, reject) {
+    let pdfParser = new PDFParser(this, 1)
+    pdfParser.on("pdfParser_dataReady", function(evtData) {
+      // let meta = pdfParser.prototype.loadMetaData ()
+      // log('__M', meta)
+      let str = pdfParser.getRawTextContent()
+      str = str.replace(/\r/g, '')
+      let rows = str.split('\n')
+      let pages = [], page = [], clean
+      rows.forEach(row=> {
+        if (/Page.*Break/.test(row)) {
+          if (page.length) pages.push(page)
+          page = []
+        } else {
+          clean = row.trim()
+          if (!clean) return
+          clean = cleanStr(clean)
+          page.push(clean)
+        }
+      })
+      // pages = pages.slice(68, 71)
 
-//       // remove possible colons:
-//       let has_colon = false
-//       let singles = cleans.map(page=> page[0])
-//       let uniq = _.uniq(singles)
-//       if (singles.length/uniq.length > 10) has_colon = true
-//       // log('_HAS_COLON_', singles.length, uniq.length, has_colon)
-//       let freqs = []
-//       if (has_colon) {
-//         for (let colon of uniq) {
-//           freqs.push({colon, freq: countInArray(singles, colon)})
-//         }
-//         let max = _.max(freqs, 'freq')
-//         let colon = max.colon
+      // remove digits-only colons:
+      let cleans = [], test
+      for (let page of pages) {
+        test = page[0].replace(/-/g, '').trim()
+        if (/^\d+$/.test(test)) page = page.slice(1)
+        test = page[page.length-1].replace(/-/g, '').trim()
+        if (/^\d+$/.test(test)) page = page.slice(0, -1)
+        cleans.push(page)
+      }
 
-//         pages = []
-//         for (let clean of cleans) {
-//           if (clean[0] == colon) page = clean.slice(1)
-//           pages.push(page)
-//         }
-//       } else {
-//         pages = cleans
-//       }
-//       pages.forEach(page=> {
-//         page[0] = 'HEAD-' + page[0]
-//       })
-//       let strs = _.flatten(pages)
-//       let text = strs.join('\n')
+      // remove possible colons:
+      let has_colon = false
+      let singles = cleans.map(page=> page[0])
+      let uniq = _.uniq(singles)
+      if (singles.length/uniq.length > 10) has_colon = true
+      // log('_HAS_COLON_', singles.length, uniq.length, has_colon)
+      let freqs = []
+      if (has_colon) {
+        for (let colon of uniq) {
+          freqs.push({colon, freq: countInArray(singles, colon)})
+        }
+        let max = _.max(freqs, 'freq')
+        let colon = max.colon
 
-//       let pars = breakRow(text)
-//       let titles = pars.filter(par=> /HEAD/.test(par) && par.length < 50)
-//       let docs = pars.map(par=> {
-//         let doc = {md: par}
-//         if (/HEAD/.test(par) && par.length < 50) {
-//           doc.level = 2
-//           doc.md = par.slice(5)
-//         }
-//         return doc
-//       })
-//       let descr = {title: 'xxx', auth: 'xxx'}
-//       let res = {descr, docs, imgs: []}
-//       resolve(res)
-//     })
-//     pdfParser.on("pdfParser_dataError", function(evtData) {
-//       log('____ERR:')
-//     })
-//     pdfParser.loadPDF(bpath)
-//   })
-// }
+        pages = []
+        for (let clean of cleans) {
+          if (clean[0] == colon) page = clean.slice(1)
+          pages.push(page)
+        }
+      } else {
+        pages = cleans
+      }
+      pages.forEach(page=> {
+        page[0] = 'HEAD-' + page[0]
+      })
+      let strs = _.flatten(pages)
+      let text = strs.join('\n')
+
+      let pars = breakRow(text)
+      let titles = pars.filter(par=> /HEAD/.test(par) && par.length < 50)
+      let docs = pars.map(par=> {
+        let doc = {md: par}
+        if (/HEAD/.test(par) && par.length < 50) {
+          doc.level = 2
+          doc.md = par.slice(5)
+        }
+        return doc
+      })
+      let descr = {title: 'xxx', auth: 'xxx'}
+      let res = {descr, docs, imgs: []}
+      resolve(res)
+    })
+    pdfParser.on("pdfParser_dataError", function(evtData) {
+      log('____ERR:')
+    })
+    pdfParser.loadPDF(bpath)
+  })
+}
 
 function cleanStr(str) {
   if (!str) return ''
